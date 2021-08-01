817db26ca3245dda0439fdbdf14533036b674c53
santiagopas <santiagopasciullo@gmail.com>
2021-08-01 12:25:02 -0300

    componenteHeader, Homepage, logo , imagenes y directory

---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory from '../../components/Directory';
+
+const Homepage = props => {
+  return(
+    <section>
+      <Directory></Directory>
+    </section>
+
+  );
+};
+
+export default Homepage;
